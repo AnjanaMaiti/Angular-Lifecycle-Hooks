598f2dad39ef7c30d61439312dfd3e08a27a1ca7
AnjanaMaiti <anjanamaiti15@gmail.com>
2020-12-29 00:02:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy, AfterContentInit, AfterViewInit {
   title = 'Angular-Lifecycle-Hooks';
+  constructor(){
+    console.log("constructor");
+  }
+  ngOnInit(){
+    console.log("OnInit");
+  }
+  ngAfterContentInit(){
+    console.log("AfterContentInit");
+  }
+  ngAfterViewInit(){
+    console.log("AfterViewInit");
+  }
+  ngOnDestroy(){
+    console.log("OnDestroy");
+  }
 }
